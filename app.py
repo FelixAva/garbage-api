31932989bdac1f63dc17c736520b2c5ab0c9b758
Felix <felavalos@gmail.com>
2025-04-04 16:18:30 +0900

    First endpoint
    Connection with snowflake
    Install dotenv-python for secret variables
    Categories endpoint

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+from db import get_snowflake_connection
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return "API Flask connected to Snowflake"
+
+@app.route('/categories')
+def get_categories():
+  conn = get_snowflake_connection()
+  cursor = conn.cursor()
+  cursor.execute("SELECT * FROM categories")
+  results = cursor.fetchall()
+  cursor.close()
+  conn.close()
+
+  categories = [{"id": row[0], "name": row[1]} for row in results]
+  return jsonify(categories)
+
+if __name__ == '__main__':
+  app.run(debug=True)
